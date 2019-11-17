d21d714cd33bbecbb9234aa58f8691dd2f8b4d19
Axel <axeho681@student.liu.se>
2019-11-17 14:32:37 +0100

    Påvbörjat del 4 av ass. 2

--- Lab 1/Assignment_2.R
+++ Lab 1/Assignment_2.R
@@ -19,7 +19,6 @@ print(likelihoodlog(1,machines))
 #Plot curve
 curve(likelihoodlog(x,machines),xlim=c(0,4), ylim=c(0,60) , col="blue")
 
-
 #Min theta. Comes from deriving the loglikelihood-function. The more data I have the more exactly I can pinpoint the exact point of failure
 minvalue = dim(machines)/sum(machines)
 print("Min θ:")
@@ -29,3 +28,13 @@ print((dim(machines)[1])/sum(machines))
 #3
 curve(likelihoodlog(x,machines[1:6,]), from=0, to=4, col="red", add = TRUE)
 print((dim(machines[1:6,])[1])/sum(machines[1:6,]))
+
+#4
+#TODO NEDAN ÄR FEL! KÖR ENDAST LIKELIHOOD PÅ BETINGADE FUNKTIONEN
+bayesianfunc = function(x, theta) {
+  return(log(theta*exp(-theta*x)*10*exp(-10*theta)))
+} 
+
+print(bayesianfunc(machines,1))
+
+curve(bayesianfunc(machines, x), xlim=c(0,4), col="green")
