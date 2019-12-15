7760e3df0d4f35bdfad92bcdffd2d4795f98ef1f
Axel Holmberg <axeho681@student.liu.se>
2019-12-15 13:20:05 +0100

    First iteration with all kernels finished

--- Lab 3/Assignment_1.R
+++ Lab 3/Assignment_1.R
@@ -8,8 +8,6 @@ stations <- read.csv("stations.csv")
 temps <- read.csv("temps50k.csv")
 st <- merge(stations, temps, by = "station_number")
 
-
-
 ## These three values are up to the user
 a <- 58.4274 # The lat of the point to predict
 b <- 14.826 # The long of the point to predict
@@ -18,7 +16,7 @@ date <-"2013-11-04" # The date to predict (up to the students)
 times <- c("04:00:00", "06:00:00","06:00:00","08:00:00","10:00:00","12:00:00",
            "14:00:00","16:00:00","18:00:00","20:00:00", "22:00:00", "24:00:00")
 
-p1 <-  c(st$latitude,st$longitude)
+p1 <-  c(st$latitude[1],st$longitude[1])
 p2 <- c(a,b)
 
 
@@ -35,16 +33,32 @@ filterPosteriorTime <- function(data, time, date) {
 }
 
 #Kernel for distance computing to point of interest
-distCompute <- function(p1, p2) {
-  
-  return (distHaversine(p1,p2));
+distGaussian <- function(p1, p2, h) {
+  return (exp(-(distHaversine(p1,p2)/h)**2));
 }
 
-distCompute(p1,p2)
+#Kenrel for date
+dateGaussian <- function(p1, p2, h) {
+  return(exp(-((as.numeric(as.Date(p1))-as.numeric(as.Date(p2)))/h)**2))
+}
 
-h_distance <- 
-h_date <-
-h_time <-
+timeGaussian <- function(p1,p2,h) {
+  return(exp(-((difftime(strptime(p1, format="%H:%M:%S"), strptime(p2, format="%H:%M:%S"))))**2)/h)
+}
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+
+h_distance <- 500000
+h_date <- 5
+h_time <- 2
 
 temp <- vector(length = length(times)) # Students’ code here
 plot(temp, type = "o")
