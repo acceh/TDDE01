6b892cdbbeaa981daa0cd85bba0fb48169de17fa
Axel Holmberg <axeho681@student.liu.se>
2019-12-15 11:59:33 +0100

    Changed structure a bit

--- Lab 3/Assignment_1.R
+++ Lab 3/Assignment_1.R
@@ -7,9 +7,17 @@ library("geosphere")
 stations <- read.csv("stations.csv")
 temps <- read.csv("temps50k.csv")
 st <- merge(stations, temps, by = "station_number")
-# These three values are up to the students
-a <- 58.4274 # The point to predict (up to the students) 
-b <- 14.826
+
+
+
+## These three values are up to the user
+a <- 58.4274 # The lat of the point to predict
+b <- 14.826 # The long of the point to predict
+date <-"2013-11-04" # The date to predict (up to the students) 
+##End input
+times <- c("04:00:00", "06:00:00","06:00:00","08:00:00","10:00:00","12:00:00",
+           "14:00:00","16:00:00","18:00:00","20:00:00", "22:00:00", "24:00:00")
+
 p1 <-  c(st$latitude,st$longitude)
 p2 <- c(a,b)
 
@@ -26,8 +34,6 @@ filterPosteriorTime <- function(data, time, date) {
                  strptime(time, format="%H:%M:%S"))) > 0 ),])
 }
 
-
-
 #Kernel for distance computing to point of interest
 distCompute <- function(p1, p2) {
   
@@ -40,6 +46,5 @@ h_distance <-
 h_date <-
 h_time <-
 
-date <-"2013-11-04" # The date to predict (up to the students) times <- c("04:00:00", "06:00:00", ..., "24:00:00")
 temp <- vector(length = length(times)) # Students’ code here
 plot(temp, type = "o")
