b433ccf47a0041d9a1966a48ea5a8291025852b7
Axel <axel@Malins-MacBook-Pro.local>
2019-12-17 11:48:32 +0100

    Almost finished with report and labs. Some small changes may be needed

--- Lab 3/Assignment_1.R
+++ Lab 3/Assignment_1.R
@@ -11,7 +11,7 @@ st <- merge(stations, temps, by = "station_number")
 ## These values are up to the user. ud = user defined
 ud.lat <- 59.325 # The lat of the point to predict
 ud.long <- 18.071 # The long of the point to predict
-ud.date <-"2016-12-24" # The date to predict (up to the students) 
+ud.date <-"2001-12-24" # The date to predict (up to the students) 
 ud.h_distance <- 100000
 ud.h_date <- 30
 ud.h_time <- 4
@@ -60,6 +60,9 @@ filtered_data <- filterPosteriorDate(st, ud.date)
 length = length(ud.times)
 t_sum <- vector(length = length)
 t_multi <- vector(length = length)
+t_summa_test <- vector(length = length)
+t_multi_test<- vector(length = length)
+
 for (i in 1:length) {
   print(i)
   filtered_data_by_time <-
@@ -73,15 +76,13 @@ for (i in 1:length) {
   day <- dateGaussian(filtered_data_by_time, ud.date, ud.h_date)
   kernel_sum <- distance + day + time
   kernel_multi <- distance * day * time
+  t_summa_test <- kernel_sum
+  t_multi_test <- kernel_multi
   t_sum[i] <-
     sum(kernel_sum %*% filtered_data_by_time$air_temperature) / sum(kernel_sum)
   t_multi[i] <-
     sum(kernel_multi %*% filtered_data_by_time$air_temperature) / sum(kernel_multi)
 }
-plot(1:length(distance), distance, main = "Distance")
-plot(1:length(day), day, main = "Day")
-plot(1:length(time), distance, main = "Time")
-
 
 temps <- list(t_sum=t_sum, t_multi=t_multi)
 
@@ -92,3 +93,37 @@ axis(1, at=1:length(ud.times), labels=ud.times)
 plot(temps$t_multi,xaxt='n', xlab="Time", 
      ylab="Temperature", type="o", main = "Multiplication of kernels")
 axis(1, at=1:length(ud.times), labels=ud.times)
+
+
+#To evaaluate h-values
+plot(1:length(distance), distance, main = "Distance")
+plot(1:length(day), day, main = "Day")
+plot(1:length(time), distance, main = "Time")
+
+
+#Below is the h-tests
+h_test.plotKernalDistance <- function(distances, h) {
+  u <- distances/h
+  k <- exp(-u^2)
+  plot(k, type="l", main = "H value for distance", xlab="Distance",)
+}
+
+h_test.plotKernalDate <- function(date_diff, h) {
+  u <- date_diff/h
+  k <- exp(-u^2)
+  plot(k, type="l", main = "H value for date", xlab="Days",)
+}
+
+h_test.plotKernalHour <- function(time_diff, h) {
+  u <- time_diff/h
+  k <- exp(-u^2)
+  plot(k, type="l", main = "H value for hour", xlab="Hours", xlim=c(0,12))
+}
+
+h_test.distance <- seq(0,300000,1)
+h_test.date_diff <- seq(0,20,1)
+h_test.time_diff <- seq(0,50,1)
+
+h_test.plotKernalDistance(h_test.distance, ud.h_distance)
+h_test.plotKernalDate(h_test.date_diff, ud.h_date)
+h_test.plotKernalHour(h_test.time_diff, ud.h_time)
